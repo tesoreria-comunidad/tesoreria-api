5f4c3be0e38c6b423e37c4b91febfaf8744a6961
Gaston Sagasti <gastisagasti@gmail.com>
2025-08-18 15:56:42 -0300

    import manual del nuevo modulo

--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -5,8 +5,10 @@ import { UserService } from 'src/user/user.service';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
+import { ServicesModule } from 'src/services/services.module';
 
 @Module({
+  imports: [ServicesModule],
   controllers: [PersonController],
   exports: [PersonService],
   providers: [
